5ad394c358c47ced792ff2d41a1e0c9b0775b8c1
jacobbayp <jacobbayp@gmail.com>
2021-04-26 10:20:18 +0200

    added settings.js

---
+++ src/settings.js
@@ -0,0 +1,8 @@
+//const URL = "http://localhost:8080/ca3/api/";
+const URL = "https//:www.test.madebay.dk/tomcat/ca3/api/"
+
+const adminURL = URL + "info/admin";
+const userURL = URL + "info/user";
+const fiveThingsURL = URL + "5things/teachersSolution";
+
+export {adminURL, userURL, fiveThingsURL};
